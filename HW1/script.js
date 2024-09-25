33f4c1a93b4c2d310c703359f06084136f07b1d7
CherchenkoMihail <cherchenko.mihail@gmail.com>
2024-09-26 02:10:57 +0300

    HW1

---
+++ HW1/script.js
@@ -0,0 +1,121 @@
+var numb1 = 1;
+var text1 = 'Hello World';
+var numb2 = 2;
+var text2 = 'Apple';
+var numb3 = 3;
+
+let text3 = 'Champions League';
+let numb4 = 4;
+let text4 = 'Java Script';
+let numb5 = 5;
+let text5 = 'Full Stack';
+
+const numb6 = 6;
+const text6 = 'NBA';
+const numb7 = 7;
+const text7 = 'Music';
+const numb8 = 8;
+
+console.log(numb1)
+console.log(text1)
+console.log(numb2)
+console.log(text2)
+console.log(numb3)
+
+console.log(text3)
+console.log(numb4)
+console.log(text4)
+console.log(numb5)
+console.log(text5)
+
+console.log(numb6)
+console.log(text6)
+console.log(numb7)
+console.log(text7)
+console.log(numb8)
+
+document.write(numb1)
+document.write('<br>')
+
+document.write(text1)
+document.write('<br>')
+
+document.write(numb2)
+document.write('<br>')
+
+document.write(text2)
+document.write('<br>')
+
+document.write(numb3)
+document.write('<br>')
+
+document.write(text3)
+document.write('<br>')
+
+document.write(numb4)
+document.write('<br>')
+
+document.write(text4)
+document.write('<br>')
+
+document.write(numb5)
+document.write('<br>')
+
+document.write(text5)
+document.write('<br>')
+
+document.write(numb6)
+document.write('<br>')
+
+document.write(text6)
+document.write('<br>')
+
+document.write(numb7)
+document.write('<br>')
+
+document.write(text7)
+document.write('<br>')
+
+document.write(numb8)
+document.write('<br>')
+
+document.write('<hr>')
+
+alert(numb1)
+alert(text1)
+alert(numb2)
+alert(text2)
+alert(numb3)
+
+confirm(text3)
+confirm(numb4)
+confirm(text4)
+confirm(numb5)
+confirm(text5)
+
+prompt(numb6)
+prompt(text6)
+prompt(numb7)
+prompt(text7)
+prompt(numb8)
+
+document.write('var - це старий спосіб оголошення змінних. Значення змінної може буди визначенно пізніше.')
+document.write('<br>')
+
+document.write('let - це новий спосіб оголошення змінних. Значення змінної може буди визначенно пізніше.')
+document.write('<br>')
+
+document.write('const - також оголошує змінні, але значення цієї змінної не може бути змінене після ініціалізації. І значення має зазначатись одразу.')
+document.write('<br>')
+
+document.write('Декларація - це оголошення змінної.')
+document.write('<br>')
+
+document.write('Ініціалізація - це присвоєння значення змінної.')
+document.write('<hr>')
+
+
+
+
+
+
